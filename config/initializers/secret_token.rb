506a3346f618f1d30a8ab3d632d4dc02be1d2141
Quy Phan <quyp@qualcomm.com>
2012-10-23 11:40:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfoliolizer::Application.config.secret_token = '7a1bc2f4c7afa3c2890f45c5a26e5e4ff338612a53ebf06775b739e448338a707fafe495a97de8f3d59cd769973a2427f05540297b384cba65d3700be7166515'
